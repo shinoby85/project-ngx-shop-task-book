5b79c57d635243a83877238ac5b80c5799c3b7d9
shinoby85 <ponk@tut.by>
2021-05-26 16:08:53 +0300

    Create delegate 2

--- 3-directives/2-start-rating-component/src/app/product-card/star-rating/star-rating.component.ts
+++ 3-directives/2-start-rating-component/src/app/product-card/star-rating/star-rating.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'ngx-shop-star-rating',
   templateUrl: './star-rating.component.html',
 })
 export class StarRatingComponent {
+  @Input()
+  public feedbackRate: number = 0
+
+  public stars: number[] = [0, 1, 2, 3, 4]
+
+  public highlight(ind) {
+    return (ind + 1) <= Math.round(this.feedbackRate)
+  }
+
 
 }
