6dcc5cc4b6eff8e0c165f4878f741d0876eb3b2c
shinoby85 <ponk@tut.by>
2021-05-26 17:01:54 +0300

    Start task 3 directive

--- 3-directives/3-information-component/src/app/information/information.component.ts
+++ 3-directives/3-information-component/src/app/information/information.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from "../../../../../shared/mocks/3-directives/product-information";
 
 @Component({
   selector: 'ngx-shop-information',
   templateUrl: './information.component.html',
 })
-export class InformationComponent {}
+export class InformationComponent {
+  @Input()
+  public product:IProduct;
+
+  @Output()
+  public addToCart: EventEmitter<any>=new EventEmitter<any>()
+
+
+
+}
