fbc82965d8436c72d8d5f040fe4e16e51a13ebf7
shinoby85 <ponk@tut.by>
2021-05-27 17:59:47 +0300

    Finish task 4 directive

--- 3-directives/4-description-component/src/app/description/feedbacks/feedbacks.component.ts
+++ 3-directives/4-description-component/src/app/description/feedbacks/feedbacks.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {IFeedback, IProduct} from "../../../../../../shared/mocks/3-directives/product-information";
 
 @Component({
   selector: 'ngx-shop-feedbacks',
   templateUrl: './feedbacks.component.html',
   styleUrls: ['./feedbacks.component.sass'],
 })
-export class FeedbacksComponent {}
+export class FeedbacksComponent{
+  @Input()
+  public feedbacks: IFeedback[] = []
+
+  @Output()
+  public addFeedbackEvent: EventEmitter<string>=new EventEmitter<string>()
+
+
+  public addNewFeedback() {
+    this.addFeedbackEvent.emit("отзыв добавлен")
+  }
+}
