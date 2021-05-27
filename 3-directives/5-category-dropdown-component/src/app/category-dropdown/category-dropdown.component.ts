96a3cc5e9b9ebc7da2fe31a9cf91a65d0a0a8308
shinoby85 <ponk@tut.by>
2021-05-27 18:48:49 +0300

    Finish task 5 directive

--- 3-directives/5-category-dropdown-component/src/app/category-dropdown/category-dropdown.component.ts
+++ 3-directives/5-category-dropdown-component/src/app/category-dropdown/category-dropdown.component.ts
@@ -1,7 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ICategory} from "../../../../../shared/mocks/3-directives/categories";
 
 @Component({
   selector: 'ngx-shop-category-dropdown',
   templateUrl: './category-dropdown.component.html',
 })
-export class CategoryDropdownComponent {}
+export class CategoryDropdownComponent {
+  @Input()
+  public categories: ICategory[] = []
+
+  @Output()
+  public subCategorySelectEvent: EventEmitter<string> = new EventEmitter<string>()
+
+  public currentIndex: number | null = null
+  public currentCategory = ""
+
+
+  public subCategorySelect(subCat: string) {
+    this.subCategorySelectEvent.emit(`Переход на категорию товара: ${subCat}`)
+  }
+
+  showSubCategories(ind) {
+    this.currentIndex = ind
+  }
+
+  setSubCategory(_id: string) {
+    this.currentCategory=_id;
+  }
+
+  moveSubCategory() {
+    this.currentCategory=""
+  }
+}
